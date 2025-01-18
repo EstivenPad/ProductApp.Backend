d04c994e3fa8bd6736f678fa10b7d402102d99d4
EstivenPad <estivenps26@gmail.com>
2025-01-17 22:27:39 -0400

    Adding domain entities

---
+++ ProductApp.Core/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApp.Core
+{
+    public class Color
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
---
+++ ProductApp.Core/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApp.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int ColorId { get; set; }
+        public Color Color { get; set; }
+    }
+}
