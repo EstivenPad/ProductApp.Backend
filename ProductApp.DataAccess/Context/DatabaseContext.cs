2c73efb07e1f352b948c8e55eeba2ad064c5b6c3
EstivenPad <estivenps26@gmail.com>
2025-01-17 22:45:11 -0400

    Adding DbContext

---
+++ ProductApp.DataAccess/Context/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Core;
+
+namespace ProductApp.DataAccess.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Color> Color { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
